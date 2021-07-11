455c452ca8d05c5bbd258d71b7de773a6107423e
HornyFerret <ombro.de.morto@gmail.com>
2021-07-11 17:58:28 +0300

    Добавьте файлы проекта.

---
+++ codewars-8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace codewars_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            Console.WriteLine(num);
+            Console.WriteLine("-----");
+            int w = summation(num);
+            Console.WriteLine(w);
+        }
+        public static int summation(int num)
+        { 
+            int m = 1;
+            for (int i = 1; i < num; i++)
+            {
+                m = m + (i + 1);
+            }
+            return m;
+        }
+    }
+}
